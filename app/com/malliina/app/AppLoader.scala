79fd1d9a715b3b603d8c4187f30bfff901bc3027
Michael Skogberg <malliina123@gmail.com>
2021-01-17 18:08:42 +0200

    Up deps

--- app/com/malliina/app/AppLoader.scala
+++ app/com/malliina/app/AppLoader.scala
@@ -2,13 +2,18 @@ package com.malliina.app
 
 import controllers.{AssetsComponents, Home}
 import play.api.ApplicationLoader.Context
-import play.api.{Application, ApplicationLoader, BuiltInComponentsFromContext}
+import play.api.{Application, ApplicationLoader, BuiltInComponentsFromContext, LoggerConfigurator}
 import play.api.routing.Router
 import play.filters.HttpFiltersComponents
 import router.Routes
 
 class AppLoader extends ApplicationLoader {
-  override def load(context: Context): Application = new AppComponents(context).application
+  override def load(context: Context): Application = {
+    val environment = context.environment
+    LoggerConfigurator(environment.classLoader)
+      .foreach(_.configure(environment))
+    new AppComponents(context).application
+  }
 }
 
 class AppComponents(context: Context)
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,13 +1,13 @@
-scalaVersion := "2.12.10"
+scalaVersion := "2.12.12"
 
 scalacOptions ++= Seq("-unchecked", "-deprecation")
 
 // for vanilla
 Seq(
-  "com.typesafe.play" % "sbt-plugin" % "2.8.2",
+  "com.typesafe.play" % "sbt-plugin" % "2.8.7",
   "com.typesafe.sbt" % "sbt-digest" % "1.1.4",
   "com.typesafe.sbt" % "sbt-gzip" % "1.0.2",
   "com.eed3si9n" % "sbt-buildinfo" % "0.9.0",
-  "ch.epfl.scala" % "sbt-bloop" % "1.4.1",
-  "org.scalameta" % "sbt-scalafmt" % "2.4.0"
+  "ch.epfl.scala" % "sbt-bloop" % "1.4.4",
+  "org.scalameta" % "sbt-scalafmt" % "2.4.2"
 ) map addSbtPlugin
